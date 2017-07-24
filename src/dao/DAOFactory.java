ba325f1b25ead7688bf6deb19f173e14a9719b9a
steven.liatti <steven.liatti@etu.hesge.ch>
2017-07-24 14:16:00 +0200

    Update the DAOFactory to reuse the pool connection
    Changes to be committed:
    	modified:   src/dao/DAOFactory.java

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -1,23 +1,21 @@
 package dao;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import com.jolbox.bonecp.BoneCP;
+import com.jolbox.bonecp.BoneCPConfig;
+
 public class DAOFactory {
 	private static final String PROPERTIES_FILE = "/dao/dao.properties";
+	BoneCP connectionPool = null;
 	
-	private String url;
-	private String username;
-	private String password;
-	
-	public DAOFactory(String url, String username, String password) {
-		this.url = url;
-		this.username = username;
-		this.password = password;
+	DAOFactory(BoneCP connectionPool) {
+		this.connectionPool = connectionPool;
 	}
 	
 	public static DAOFactory getInstance() throws DAOConfigurationException {
@@ -28,6 +26,7 @@ public class DAOFactory {
         String driver;
         String username;
         String password;
+        BoneCP connectionPool = null;
         
         if (propertiesFile == null) {
 			throw new DAOConfigurationException("file " + PROPERTIES_FILE + " not found");
@@ -39,6 +38,8 @@ public class DAOFactory {
 			driver = properties.getProperty("driver");
 			username = properties.getProperty("username");
 			password = properties.getProperty("password");
+        } catch (FileNotFoundException e) {
+	            throw new DAOConfigurationException("The file " + PROPERTIES_FILE + " is not found.", e);
 		} catch (IOException e) {
 			throw new DAOConfigurationException("Impossible to load the properties file " + PROPERTIES_FILE, e);
 		}
@@ -49,12 +50,28 @@ public class DAOFactory {
 			throw new DAOConfigurationException("driver not found in classpath", e);
 		}
         
-        DAOFactory instance = new DAOFactory(url, username, password);
+        try {
+        	BoneCPConfig config = new BoneCPConfig();
+            config.setJdbcUrl(url);
+            config.setUsername(username);
+            config.setPassword(password);
+
+            config.setMinConnectionsPerPartition(5);
+            config.setMaxConnectionsPerPartition(10);
+            config.setPartitionCount(2);
+
+            connectionPool = new BoneCP( config );
+		} catch (SQLException e) {
+			e.printStackTrace();
+            throw new DAOConfigurationException("Error configuration connections' pool.", e);
+		}
+        
+        DAOFactory instance = new DAOFactory(connectionPool);
         return instance;
 	}
 	
 	Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(url, username, password);
+		return connectionPool.getConnection();
 	}
 	
 	public CandidateDAO getCandidateDao() {
