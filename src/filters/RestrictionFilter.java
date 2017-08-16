99d6c722d41aaa6edda4b3115de9884a4d91655a
steven.liatti <steven.liatti@etu.hesge.ch>
2017-08-16 11:45:49 +0200

    Forget two functions in filter
    Changes to be committed:
    	modified:   src/filters/RestrictionFilter.java

--- src/filters/RestrictionFilter.java
+++ src/filters/RestrictionFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -12,6 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class RestrictionFilter implements Filter {
+	
+	public void init(FilterConfig config) throws ServletException {
+
+    }
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
@@ -35,5 +40,9 @@ public class RestrictionFilter implements Filter {
 			chain.doFilter(request, response);
 		}
 	}
+	
+	public void destroy() {
+
+    }
 
 }
